2ce669555efb4db0793c3a7a380c32049875c6bc
Estar0622 <myjudai@naver.com>
2024-07-07 20:27:15 +0900

    feat: 예시 추가

--- src/main/java/org/example/silver/S4_정렬하기.java
+++ src/main/java/org/example/silver/S4_정렬하기.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class S4_정렬하기 {
     public static void main(String[] args) {
         List<String> li = new ArrayList<>();
+        li.add("1");
 
+        System.out.println(li);
     }
 }
