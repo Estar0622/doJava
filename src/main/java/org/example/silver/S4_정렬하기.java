20375cf578ce0bf75865b26b44860e7c8730af9d
estar <estar@Estar-MacBook-Air.local>
2024-07-07 20:17:45 +0900

    feat: li 추가함

---
+++ src/main/java/org/example/silver/S4_정렬하기.java
@@ -0,0 +1,11 @@
+package org.example.silver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class S4_정렬하기 {
+    public static void main(String[] args) {
+        List<String> li = new ArrayList<>();
+
+    }
+}
